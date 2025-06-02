93ce5b151173ae99e3d7b78872d32aaea3dfaaf2
RickFDND <rick.pijnacker@hva.nl>
2025-06-02 21:07:18 +0200

    Express installed en public map aangemaakt. #2

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
 console.log('Hier komt je server voor Sprint 12.')
+//express downloaden
+import express from 'express'
+
+const app = express()
+
+//public map
+app.use(express.static('public'))
+
+//laat data beter ophalen
+app.use(express.urlencoded({extended: true}))
+
+
+
+
+
+//redirect naar homepage als pagina niet werkt
+app.post('/', async function (request, response) {
+ 
+  response.redirect(303, '/')
+})
+
+//nummer van localhost
+app.set('port', process.env.PORT || 8000)
+
+//foutmelding 
+app.listen(app.get('port'), function () {
+  
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
